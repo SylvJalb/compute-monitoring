0f157ca0df96e5fb36e501d6d60d492effa93e7f
Romain Royer <romain.royer@eisti.fr>
2021-04-03 16:52:10 +0200

    Update MultiProcessing.c

--- src/MultiProcessing.c
+++ src/MultiProcessing.c
@@ -43,7 +43,8 @@ void pere(int* numLect, int nbLect) {
         printf("Père     \t(mon id -> %d)\n", getpid());
 
         close(tube[1]); // Fermeture lecture
-        close(tubeMonkey[0]); // Ouverture lecture
+
+        close(tubeMonkey[0]); // Fermeture lecture
 
         char buf[BUF_SIZE];
         while(read(tube[0], buf, sizeof(buf))!=0) {
@@ -75,16 +76,17 @@ void pere(int* numLect, int nbLect) {
 
             size = sizeof(child_list) / sizeof(child_list[0]);
 
-            sprintf(str_monkey, "%d_",size);
+            sprintf(str_monkey, "%d",size);
 
             // Affichage des processus fils
             for (int j = 0; j < size; j++) {
                 printf("%d\n", child_list[j]);
-                sprintf(str_monkey + strlen(str_monkey), "|%d",child_list[j]);
+                sprintf(str_monkey + strlen(str_monkey), "-%d",child_list[j]);
             }
+
             write(tubeMonkey[1], str_monkey, BUF_SIZE_MONKEY);
-            printf("Le message à envoyer %s\n",str_monkey);
-            close(tubeMonkey[1]);
+
+            //printf("Le message à envoyer %s\n",str_monkey);
 
             printf("%s", buf);
             
@@ -106,23 +108,69 @@ void pere(int* numLect, int nbLect) {
 }
 
 void evilMonkey(){
-    char str_monkey[BUF_SIZE_MONKEY];
-    sleep(5);
+    // Variable aléatoire : temps de lancement du evil monkey (le x de la consigne)
+    int sleepTime = alea(10); 
+    //printf("Hmmm... je dors pendant %d secondes\n", sleepTime);
+    sleep(sleepTime);
 
-    close(tubeMonkey[1]); // Ouverture lecture
 
-    char buf_Monkey[BUF_SIZE_MONKEY];
+    close(tubeMonkey[1]); // Ouverture du pipe Père -> Evil Monkey lecture
 
-    if(read(tubeMonkey[0], buf_Monkey, sizeof(buf_Monkey))!=0) {
-        printf("Message recu : %s\n", str_monkey);
+    int compteur = 1; //Compteur pour lancer la lecture du pipe uniquement sur les dernières informations reçues (toutes les mêmes)
+    int x = 0; //element extrait de message du pipe
+    int passage = 0; //permet de savoir si on a déjà iniatialisé le nombre de processus.
+    int *elements; //tableau des fils (leur PID)
+    char buf_Monkey[BUF_SIZE_MONKEY]; //message (char) stocké dans le buffer du Evil Monkey
+    int size = 0; //taille du tableau élements (ou nombre de processus)
+    int reponse; //variable de retour pour le SIGKILL
+    void erreur(const char *); //void de gestion d'erreur
+    char delim[] = "-"; //delimiter utilisé dans le message du pipe (séparant les infos envoyées par le père)
+
+    while(read(tubeMonkey[0], buf_Monkey, sizeof(buf_Monkey))!=0) { //ouverture en lecture du pipe
+        if(compteur == sleepTime){
+            char *ptr = strtok(buf_Monkey,delim);
+            while(ptr != NULL){
+
+                x = atoi(ptr);
+
+                if(passage == 0){
+                    elements = creerTableauEntier(x);
+                    size = x;
+                }
+                else{
+                    elements[passage-1] = x;
+                }
+                passage ++;
+                //printf("\t'%s'\n",ptr);
+                ptr = strtok(NULL,delim);
+            }
+
+            for(int i=0; i<size; i++){
+                printf("element : %d %d\n ", elements[i], i);
+            }
+            compteur = 1;
+
+            int targetedSonNumber = alea(size);
+            int targetedSon = elements[targetedSonNumber];
+
+            printf("\nL'evil monkey va frapper le fils %d !\n", targetedSon);
+            if ((reponse = kill(targetedSon,SIGKILL)) == -1)
+                erreur("SIGKILL kill");
+
+            printf("Le processus %d a été tué !\n",targetedSon);
+            if(size>0){
+            free(elements);
+            }
+        }
+        compteur ++;
     }
-
-    //int targetedSonNumber = alea(size); //Remplacer val par la taille de la liste des fils
-    //int targetedSon = child_list[targetedSonNumber];
-    //printf("L'evil monkey va frapper le fils %d\n", targetedSon);
-    printf("KILL");
+    close(tubeMonkey[0]);
 }
 
+void erreur(const char * message){
+    perror(message);
+    exit(-1);
+}
 
 int alea(int taille){
     int res;
